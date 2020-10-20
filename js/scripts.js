98abbb14bf0820dd06deda608e1581e50dff3e50
Alex Menlah <a.menlah@itebodoni.edu.it>
2020-10-20 14:09:50 +0200

    add pre-loader, some changes, fix noscript p.

--- js/scripts.js
+++ js/scripts.js
@@ -44,5 +44,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 
+document.onreadystatechange = function() { 
+  if (document.readyState !== "complete") { 
+      document.querySelector("body").style.visibility = "hidden"; 
+      document.querySelector("#loader-wrapper").style.visibility = "visible"; 
+  } else { 
+      document.querySelector("#loader-wrapper").style.display = "none"; 
+      document.querySelector("body").style.visibility = "visible"; 
+  } 
+};
 
 
